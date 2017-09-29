b84c727bf7970b506cb60f41a76b8c550b97c7d3
Unknown <agustingarcia1997@gmail.com>
2017-09-29 17:55:46 -0300

    Ogmo level

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -6,16 +6,23 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import entities.Player;
 import flixel.addons.display.FlxBackdrop;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
+import flixel.tile.FlxTilemap;
 
 class PlayState extends FlxState
 {
+	private var loader:FlxOgmoLoader;
 	private var player:Player;
 	private var pivot:FlxSprite;
 	private var background:FlxBackdrop;
+	private var tilemap:FlxTilemap;
 
 	override public function create():Void
 	{
 		super.create();
+		
+		loader= new FlxOgmoLoader(AssetPaths.level1GradiusGG__oel);
+		tilemap = loader.loadTilemap();
 		
 		pivot = new FlxSprite(FlxG.width / 2, FlxG.height / 2);
 		pivot.makeGraphic(1, 1, 0x00000000);
@@ -23,7 +30,7 @@ class PlayState extends FlxState
 		FlxG.camera.follow(pivot);
 		add(pivot);
 		
-		background = new FlxBackdrop(AssetPaths.wallpaper__png);
+		background = new FlxBackdrop(AssetPaths.wallpaper1__png);
 		add(background);
 		
 		player = new Player(100, 150);
